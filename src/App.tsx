3fba26e5bef2cf296a0242b8a5c8c6d607fcce08
Firstmars <115973826+Firstmars01@users.noreply.github.com>
2026-04-27 09:48:32 +0200

    Refactor App component layout and add headerz'n

--- src/App.tsx
+++ src/App.tsx
@@ -12,10 +12,16 @@ function App() {
   }
 
   return (
-    <>
-      <Normal />
-    </>
+    <div className="App">
+      <header className="App-header">
+        <h1>Bienvenue dans Escape Box</h1>
+        <p>Prêt à relever le défi ?</p>
+        <div className="button-container">
+          <button className="button">Commencer une partie</button>
+          <button className="button">Commencer mode démo</button>
+        </div>
+      </header>
+    </div>
   )
 }
-
 export default App
