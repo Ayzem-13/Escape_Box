af6f7347a6013b593f101291c1b4b66b3dff3743
Nakib <nakib.m@thecamp.fr>
2026-04-28 15:07:56 +0200

    fix music

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import '@fontsource/cinzel/700.css'
 import '@fontsource/im-fell-english-sc/400.css'
 import '@fontsource/share-tech-mono/400.css'
 import { ThemeProvider } from './theme/ThemeProvider'
+import volumeManager from './services/volumeManager'
 import './index.css'
 import App from './App.tsx'
 import PageDemo from './page/Demo/PageDemo.tsx'
@@ -15,6 +16,9 @@ import PageNormal from './page/Nomal/PageNormal.tsx'
 import PageCredits from './page/Credits/PageCredits.tsx'
 import PageTutos from './page/Tutos/PageTutos.tsx'
 import Layout from './components/Layout/Layout.tsx'
+
+// Initialize volume manager
+volumeManager;
 
 const router = createHashRouter([
   {
